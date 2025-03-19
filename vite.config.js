3566d9ef55a8cd02ef9b14adf33bb196ad7e9263
Raghupatil56 <rr.mc.bijju@gmail.com>
2025-03-19 12:07:31 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/my-portfolioo/",
+  base: "/my-portfolio/",
   plugins: [react(),
     tailwindcss()
   ], 
